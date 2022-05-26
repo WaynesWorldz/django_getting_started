aaf40c1fb6bf4c87afaa2ee52c28eaeabbf0c98c
Wayne Richard <wayne.richard@redclassic.com>
2022-05-26 11:22:08 -0400

    section 7 completed

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.forms import modelform_factory
 from .models import Meeting, Room
 
 
@@ -12,5 +12,12 @@ def rooms_list(request):
     return render(request, "meetings/rooms_list.html",
     {"rooms": Room.objects.all()})
 
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+
+def new(request):
+    form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
 
 
